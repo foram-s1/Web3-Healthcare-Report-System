5776770e3e89d1cb8e166054ad1f86c0ed737568
foram-s1 <foram.s1@ahduni.edu.in>
2022-03-26 13:16:58 +0530

    login

--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useContext, useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import { AuthContext } from "../../contexts/authContext";
 
 export default function Login() {
@@ -8,55 +9,20 @@ export default function Login() {
 
 	const { user, login } = useContext(AuthContext);
 
-	useEffect(() => {
-		if(user.logged == true){
-			window.location.href = '/';
-		}
-	}, [user]);
-
 	return (
 		<>
-			<Head>
-				<title>Login Page</title>
-			</Head>
-			<div>
-				<h1 style={{ textAlign: "center" }}>Login Page</h1>
+			<div className="d-flex flex-wrap vh-100">
+				<div className="col-lg-6 d-lg-flex d-md-flex d-none justify-content-center align-items-center col-md-12  ">
+					<img src="/assets/login.svg" className="img-fluid d-inline-ce" alt="logo" />
+				</div>
+				<div className="col-lg-6 col-md-12">
+					<div className="d-flex flex-wrap justify-content-center align-items-center">
+						<h2>Hello Again!</h2>
+					</div>
+				</div>
+
 			</div>
-			<br />
-			<div>
-				<h4 style={{ textAlign: "center" }}>
-					Username:
-					<input
-						value={username}
-						onChange={(e) => {
-							setUsername(e.target.value);
-						}}
-						type="text"
-					/>
-				</h4>
-				<h4 style={{ textAlign: "center" }}>
-					Password:
-					<input
-						value={password}
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-						type="password"
-					/>
-				</h4>
-				<h4 style={{ textAlign: "center" }}>
-					<button
-						onClick={async () => {
-							let ans: any = await login(username, password);
-							if(ans.success == false){
-								alert(ans.message);
-							}
-						}}
-					>
-						Login
-					</button>
-				</h4>
-			</div>
+			
 		</>
 	);
 }
